8286123448110931cac885e2a3f1e39eacbdff98
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-03 16:21:15 +0200

    add all the person data #7

--- src/routes/details/[personId]/+page.svelte
+++ src/routes/details/[personId]/+page.svelte
@@ -4,13 +4,25 @@
     console.log(data.person)
 </script>
 
-<h1>{data.person.nickname}</h1>
-<img >
-<p>
-    description
-</p>
+<main class="person-details">
+    <div>
+        <h1>{data.person.nickname}</h1>
+        <img src="https://fdnd.directus.app/assets/{data.person.mugshot}" alt="foto van {data.person.name}" width="300">
+        <p>
+            {data.person.bio}
+        </p>
+        <div>
+            <p>
+                {data.person.fav_hobby}
+            </p>
+            <p>
+                {data.person.fav_game}
+            </p>
+        </div>
+        <a href={data.person.website}>website</a>
+    </div>
+    <div>
+        afbeeldingen
+    </div>
+</main>
 
-<p></p>
-
-<a>website</a>
-<a>github</a>
