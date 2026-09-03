836efd03572aa8817984401ec97e8217ad5ff47d
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-03 13:03:10 +0200

    add details route page #7

---
+++ src/routes/details/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let {data} = $props();
+
+    const persons = data.persons;
+</script>
+
+{#each persons as person }
+    {person.nickname}<br>
+{/each}
+
+<h1>nickname</h1>
+<img >
+<p>
+    description
+</p>
+
+<p></p>
+
+<a>website</a>
+<a>github</a>
