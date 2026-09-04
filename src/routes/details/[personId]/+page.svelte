72e9d5334f4a6baf5c847391a18806485010d4d5
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-04 11:26:34 +0200

    move some details to the footer #7

--- src/routes/details/[personId]/+page.svelte
+++ src/routes/details/[personId]/+page.svelte
@@ -1,28 +1,37 @@
 <script>
-    let {data} = $props();
+	let { data } = $props();
 
-    console.log(data.person)
+	console.log(data.person);
 </script>
 
 <main class="person-details">
-    <div>
-        <h1>{data.person.nickname}</h1>
-        <img src="https://fdnd.directus.app/assets/{data.person.mugshot}" alt="foto van {data.person.name}" width="300">
-        <p>
-            {data.person.bio}
-        </p>
-        <div>
-            <p>
-                {data.person.fav_hobby}
-            </p>
-            <p>
-                {data.person.fav_game}
-            </p>
-        </div>
-        <a href={data.person.website}>website</a>
-    </div>
-    <div>
-        afbeeldingen
-    </div>
+	<div>
+		<h1>{data.person.nickname}</h1>
+		<img
+			src="https://fdnd.directus.app/assets/{data.person.mugshot}"
+			alt="foto van {data.person.name}"
+			width="300"
+		/>
+	</div>
+	<div>afbeeldingen</div>
 </main>
+<footer>
+	<div>
+		<p>
+			{data.person.bio}
+		</p>
+		<p>
+			{data.person.fav_hobby}
+		</p>
+		<p>
+			{data.person.fav_game}
+		</p>
+	</div>
+	<a href={data.person.website}>website</a>
+</footer>
 
+<style>
+	.person-details {
+		display: flex;
+	}
+</style>
