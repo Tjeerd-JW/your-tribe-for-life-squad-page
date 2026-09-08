91034559c61b94f2a6411e8efb39501487e27499
MaartentePoele <maarten.te.poele@hva.nl>
2026-09-08 12:32:58 +0200

    #21 Filterknoppen aangemaakt in een component en gestyled

---
+++ src/lib/components/Filter.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<nav>
+  <a href="/" class:active={$page.url.pathname === "/"}>Iedereen</a>
+  <a href="/docenten" class:active={$page.url.pathname === "/docenten"}>Docenten</a>
+  <a href="/studenten" class:active={$page.url.pathname === "/studenten"}>Studenten</a>
+</nav>
+
+<style>
+  nav {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+
+    a {
+      color: black;
+      text-decoration: none;
+      padding: 0.5rem 1rem;
+      border-radius: 99rem;
+      border: 1px solid #4b5259;
+      transition: 0.15s;
+
+      &:hover {
+        background-color: #4b5259;
+        color: white;
+      }
+    }
+
+    .active {
+      background-color: #4b5259;
+      color: white;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-    import List from "$lib/components/List.svelte";
-    let { data } = $props();
+  import Filter from "$lib/components/Filter.svelte";
+  import List from "$lib/components/List.svelte";
+  
+  let { data } = $props();
 
-    const persons = data.persons;
+  const persons = data.persons;
 </script>
 
 <h2>Selecteer een persoon uit de lijst</h2>
+<Filter />
