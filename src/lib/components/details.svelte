e89a7c34a93b467511db40f090a62f80f7b92f59
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-09 13:35:36 +0200

    move footer to component #26

---
+++ src/lib/components/details.svelte
@@ -0,0 +1,31 @@
+<script>
+	let { data } = $props();
+	let person = $derived(data.person);
+</script>
+
+<footer>
+	<div>
+		<p>
+			{person.bio ?? "ik heb geen bio ingevuld"}
+		</p>
+		<p>
+			{person.fav_hobby ?? "geen hobbies"}
+		</p>
+		<p>
+			{person.fav_game ?? "geen favoriete game"}
+		</p>
+	</div>
+
+	{#if person.website}
+		<a href={person.website} target="_blank">website</a>
+	{:else}
+		<p>ik heb geen website</p>
+	{/if}
+	{#if person.github_handle}
+		<a href="https://github.com/{person.github_handle}" target="_blank">
+			{person.github_handle}
+		</a>
+	{:else}
+		<p>geen github naam ingevuld</p>
+	{/if}
+</footer>
