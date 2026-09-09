eea7741a83167dd786927fdc568ad465e42f630a
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-09 11:34:30 +0200

    add let person so the data can be removed #11

--- src/routes/details/[personId]/+page.svelte
+++ src/routes/details/[personId]/+page.svelte
@@ -1,25 +1,26 @@
 <script>
 	let { data } = $props();
+	let person = $derived(data.person);
 </script>
 
 <main class="person-details">
 	<section>
-		{#if data.person.nickname}
-			<h1>{data.person.nickname}</h1>
+		{#if person.nickname}
+			<h1>{person.nickname}</h1>
 		{:else}
 			<h1>ik heb geen nickname</h1>
 		{/if}
 
-		{#if data.person.mugshot}
+		{#if person.mugshot}
 			<img
-				src="https://fdnd.directus.app/assets/{data.person.mugshot}?width=300"
-				alt="foto van {data.person.name}"
+				src="https://fdnd.directus.app/assets/{person.mugshot}?width=300"
+				alt="foto van {person.name}"
 				width="300"
 			/>
 		{:else}
 			<img
 				src="https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/86/Icon-Lifeweaver.png/revision/latest/scale-to-width-down/250?cb=20230411181321"
-				alt="foto van {data.person.name}"
+				alt="foto van {person.name}"
 				width="300"
 			/>
 		{/if}
@@ -28,24 +29,24 @@
 <footer>
 	<div>
 		<p>
-			{data.person.bio ?? "ik heb geen bio ingevuld"}
+			{person.bio ?? "ik heb geen bio ingevuld"}
 		</p>
 		<p>
-			{data.person.fav_hobby ?? "geen hobbies"}
+			{person.fav_hobby ?? "geen hobbies"}
 		</p>
 		<p>
-			{data.person.fav_game ?? "geen favoriete game"}
+			{person.fav_game ?? "geen favoriete game"}
 		</p>
 	</div>
 
-	{#if data.person.website}
-		<a href={data.person.website} target="_blank">website</a>
+	{#if person.website}
+		<a href={person.website} target="_blank">website</a>
 	{:else}
 		<p>ik heb geen website</p>
 	{/if}
-	{#if data.person.github_handle}
-		<a href="https://github.com/{data.person.github_handle}" target="_blank">
-			{data.person.github_handle}
+	{#if person.github_handle}
+		<a href="https://github.com/{person.github_handle}" target="_blank">
+			{person.github_handle}
 		</a>
 	{:else}
 		<p>geen github naam ingevuld</p>
