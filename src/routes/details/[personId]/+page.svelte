94661f562f6b671425a8081bedde7b84042fe345
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-09 13:35:36 +0200

    move footer to component #26

--- src/routes/details/[personId]/+page.svelte
+++ src/routes/details/[personId]/+page.svelte
@@ -26,32 +26,6 @@
 		{/if}
 	</section>
 </main>
-<footer>
-	<div>
-		<p>
-			{person.bio ?? "ik heb geen bio ingevuld"}
-		</p>
-		<p>
-			{person.fav_hobby ?? "geen hobbies"}
-		</p>
-		<p>
-			{person.fav_game ?? "geen favoriete game"}
-		</p>
-	</div>
-
-	{#if person.website}
-		<a href={person.website} target="_blank">website</a>
-	{:else}
-		<p>ik heb geen website</p>
-	{/if}
-	{#if person.github_handle}
-		<a href="https://github.com/{person.github_handle}" target="_blank">
-			{person.github_handle}
-		</a>
-	{:else}
-		<p>geen github naam ingevuld</p>
-	{/if}
-</footer>
 
 <style>
 	.person-details {
