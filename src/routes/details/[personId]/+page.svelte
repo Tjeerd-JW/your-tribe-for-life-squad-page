d740d4d1ec22f73c8383b25310b2bddd32f7aa30
MaartentePoele <maarten.te.poele@hva.nl>
2026-09-15 13:32:01 +0200

    #36 trailing slashes weggehaald

--- src/routes/details/[personId]/+page.svelte
+++ src/routes/details/[personId]/+page.svelte
@@ -16,13 +16,13 @@
 				src="https://fdnd.directus.app/assets/{person.mugshot}?width=300"
 				alt="foto van {person.name}"
 				width="300"
-			/>
+			>
 		{:else}
 			<img
 				src="https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/86/Icon-Lifeweaver.png/revision/latest/scale-to-width-down/250?cb=20230411181321"
 				alt="foto van {person.name}"
 				width="300"
-			/>
+			>
 		{/if}
 	</section>
 </main>
