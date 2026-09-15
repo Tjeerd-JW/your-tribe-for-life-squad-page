8f91320534541c7ad8f37e877abc8aa79d1cb9eb
MaartentePoele <maarten.te.poele@hva.nl>
2026-09-15 13:40:43 +0200

    #36 format code en taal van bestand aangepast

--- src/lib/components/Topbar.svelte
+++ src/lib/components/Topbar.svelte
@@ -1,40 +1,40 @@
 <script>
-    let data = $props();
-    let title = $derived(data.title);
+  let data = $props();
+  let title = $derived(data.title);
 </script>
 
 <h1>
-    {#each title.split("") as letter, i}
-        <span style="--delay: {i * 0.05}s">
-            <!-- Als letter een spatie is zet een whitespace neer -->
-            {letter === " " ? "\u00A0" : letter}
-        </span>
-    {/each}
+  {#each title.split("") as letter, i}
+    <span style="--delay: {i * 0.05}s">
+      <!-- Als letter een spatie is zet een whitespace neer -->
+      {letter === " " ? "\u00A0" : letter}
+    </span>
+  {/each}
 </h1>
 
 <style>
-    h1 {
-        grid-column: 1/-1;
-        width: 100dvw;
-        background-color: var(--primary-color);
-        text-align: center;
-        font-size: var(--font-size-2xl);
+  h1 {
+    grid-column: 1/-1;
+    width: 100dvw;
+    background-color: var(--primary-color);
+    text-align: center;
+    font-size: var(--font-size-2xl);
 
-        span {
-            display: inline-block;
-            animation: bounce 10s ease-in-out var(--delay) infinite;
-        }
+    span {
+      display: inline-block;
+      animation: bounce 10s ease-in-out var(--delay) infinite;
+    }
+  }
+
+  @keyframes bounce {
+    0%,
+    90%,
+    100% {
+      transform: translateY(0);
     }
 
-    @keyframes bounce {
-        0%,
-        90%,
-        100% {
-            transform: translateY(0);
-        }
-
-        95% {
-            transform: translateY(-12px);
-        }
+    95% {
+      transform: translateY(-12px);
     }
+  }
 </style>
