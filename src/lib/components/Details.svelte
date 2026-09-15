5c86b763236c0a78bb46b4bf4347ff366a307345
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-15 14:46:54 +0200

    add div for the grid #35

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -6,23 +6,26 @@
 	<p>
 		{person?.bio ?? "geen bio"}
 	</p>
-	<p>
-		hobby: {person?.fav_hobby ?? "geen hobbies"}
-	</p>
-	<p>
-		game: {person?.fav_game ?? "geen favoriete game"}
-	</p>
-
-	{#if person?.website}
-		<a href={person?.website} target="_blank">website</a>
-	{:else}
-		<p>ik heb geen website</p>
-	{/if}
-	{#if person?.github_handle}
-		<a href="https://github.com/{person.github_handle}" target="_blank">
-			github
-		</a>
-	{:else}
-		<p>geen github naam ingevuld</p>
-	{/if}
+	<div class="person-facts">
+		<p>
+			hobby: {person?.fav_hobby ?? "geen hobbies"}
+		</p>
+		<p>
+			game: {person?.fav_game ?? "geen favoriete game"}
+		</p>
+	</div>
+	<div class="person-links">
+		{#if person?.website}
+			<a href={person?.website} target="_blank">website</a>
+		{:else}
+			<p>ik heb geen website</p>
+		{/if}
+		{#if person?.github_handle}
+			<a href="https://github.com/{person.github_handle}" target="_blank">
+				github
+			</a>
+		{:else}
+			<p>geen github naam ingevuld</p>
+		{/if}
+	</div>
 </footer>
