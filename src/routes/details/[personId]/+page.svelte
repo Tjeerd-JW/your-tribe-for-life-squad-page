954bac153d8a23326713909ae5941566b8769879
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-17 10:58:55 +0200

    change img width to 500

--- src/routes/details/[personId]/+page.svelte
+++ src/routes/details/[personId]/+page.svelte
@@ -1,26 +1,36 @@
 <script>
-  let { data } = $props();
-  let person = $derived(data.person);
+	let { data } = $props();
+	let person = $derived(data.person);
 </script>
 
-<section class="person-details">
-  {#if person.nickname}
-    <h2>{person.nickname}</h2>
-  {:else}
-    <h2>ik heb geen nickname</h2>
-  {/if}
+<section>
+	{#if person.nickname}
+		<h2>{person.nickname}</h2>
+	{:else}
+		<h2>ik heb geen nickname</h2>
+	{/if}
 
-  {#if person.mugshot}
-    <img
-      src="https://fdnd.directus.app/assets/{person.mugshot}?width=300"
-      alt="foto van {person.name}"
-      width="300"
-    />
-  {:else}
-    <img
-      src="https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/86/Icon-Lifeweaver.png/revision/latest/scale-to-width-down/250?cb=20230411181321"
-      alt="foto van {person.name}"
-      width="300"
-    />
-  {/if}
+	{#if person.mugshot}
+		<img
+			src="https://fdnd.directus.app/assets/{person.mugshot}?width=300"
+			alt="foto van {person.name}"
+			width="300"
+		/>
+	{:else}
+		<img
+			src="https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/86/Icon-Lifeweaver.png/revision/latest/scale-to-width-down/250?cb=20230411181321"
+			alt="foto van {person.name}"
+			width="300"
+		/>
+	{/if}
 </section>
+
+<style>
+	section {
+		@media (min-width: 700px) {
+			img {
+				width: 500px;
+			}
+		}
+	}
+</style>
